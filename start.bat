1e3a5b30d98dc6e6ea4979a537ef7e363a60c1d3
Your Name <you@example.com>
2023-10-20 10:15:04 +0800

    add all code

---
+++ start.bat
@@ -0,0 +1,21 @@
+cd /d %~dp0
+
+call C:\dev\pyenv\Scripts\activate.bat
+
+goto aaa
+mkdir my_site
+cd my_site
+teedoc init
+
+rem -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+teedoc -d my_site init
+
+cd my_site
+teedoc install
+
+:aaa
+
+teedoc serve
+
+teedoc build
